7fe0b27b074976dd31257bdd58412eb2aa0ec17f
Mihai-Cristian Condrea <d4rk7355608@gmail.com>
2025-09-09 17:52:09 +0300

    feat(ads): support per-screen native ad layouts

--- app/src/main/java/com/d4rk/androidtutorials/java/ads/views/NativeAdBannerView.java
+++ app/src/main/java/com/d4rk/androidtutorials/java/ads/views/NativeAdBannerView.java
@@ -1,12 +1,15 @@
 package com.d4rk.androidtutorials.java.ads.views;
 
 import android.content.Context;
+import android.content.res.TypedArray;
 import android.util.AttributeSet;
 import android.widget.FrameLayout;
 
+import androidx.annotation.LayoutRes;
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 
+import com.d4rk.androidtutorials.java.R;
 import com.google.android.gms.ads.AdRequest;
 import com.d4rk.androidtutorials.java.ads.managers.NativeAdLoader;
 
@@ -16,19 +19,36 @@ import com.d4rk.androidtutorials.java.ads.managers.NativeAdLoader;
  */
 public class NativeAdBannerView extends FrameLayout {
 
+    private int layoutRes = R.layout.native_ad;
+
     public NativeAdBannerView(@NonNull Context context) {
         super(context);
+        init(context, null, 0);
     }
 
     public NativeAdBannerView(@NonNull Context context, @Nullable AttributeSet attrs) {
         super(context, attrs);
+        init(context, attrs, 0);
     }
 
     public NativeAdBannerView(@NonNull Context context, @Nullable AttributeSet attrs, int defStyleAttr) {
         super(context, attrs, defStyleAttr);
+        init(context, attrs, defStyleAttr);
+    }
+
+    private void init(@NonNull Context context, @Nullable AttributeSet attrs, int defStyleAttr) {
+        if (attrs != null) {
+            TypedArray a = context.obtainStyledAttributes(attrs, R.styleable.NativeAdBannerView, defStyleAttr, 0);
+            layoutRes = a.getResourceId(R.styleable.NativeAdBannerView_nativeAdLayout, R.layout.native_ad);
+            a.recycle();
+        }
     }
 
     public void loadAd(AdRequest adRequest) {
-        NativeAdLoader.load(getContext(), this);
+        NativeAdLoader.load(getContext(), this, layoutRes);
+    }
+
+    public void setNativeAdLayout(@LayoutRes int layoutRes) {
+        this.layoutRes = layoutRes;
     }
 }
