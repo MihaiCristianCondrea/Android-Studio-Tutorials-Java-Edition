e9a128e0fec16650b1d288e980f5ac77f0a8ab41
Mihai-Cristian Condrea <d4rk7355608@gmail.com>
2025-09-12 18:48:49 +0300

    Improve onboarding typography and theme selection

--- app/src/main/java/com/d4rk/androidtutorials/java/ui/screens/onboarding/ThemeFragment.java
+++ app/src/main/java/com/d4rk/androidtutorials/java/ui/screens/onboarding/ThemeFragment.java
@@ -10,6 +10,7 @@ import androidx.annotation.Nullable;
 import androidx.core.content.ContextCompat;
 import androidx.fragment.app.Fragment;
 import androidx.lifecycle.ViewModelProvider;
+import androidx.appcompat.app.AppCompatDelegate;
 
 import com.d4rk.androidtutorials.java.R;
 import com.d4rk.androidtutorials.java.databinding.FragmentOnboardingSelectionBinding;
@@ -26,13 +27,13 @@ public class ThemeFragment extends Fragment {
 
         binding.setTitle(getString(R.string.choose_your_style));
         binding.setDescription(getString(R.string.select_how_you_d_like_the_app_to_look));
-        binding.setFirstIcon(ContextCompat.getDrawable(requireContext(), R.drawable.ic_theme));
+        binding.setFirstIcon(ContextCompat.getDrawable(requireContext(), R.drawable.ic_light_mode));
         binding.setFirstTitle(getString(R.string.light_mode));
         binding.setFirstDescription(getString(R.string.light_mode_description));
-        binding.setSecondIcon(ContextCompat.getDrawable(requireContext(), R.drawable.ic_about));
+        binding.setSecondIcon(ContextCompat.getDrawable(requireContext(), R.drawable.ic_dark_mode));
         binding.setSecondTitle(getString(R.string.dark_mode));
         binding.setSecondDescription(getString(R.string.dark_mode_description));
-        binding.setThirdIcon(ContextCompat.getDrawable(requireContext(), R.drawable.ic_android));
+        binding.setThirdIcon(ContextCompat.getDrawable(requireContext(), R.drawable.ic_system_mode));
         binding.setThirdTitle(getString(R.string.follow_system));
         binding.setThirdDescription(getString(R.string.follow_system_mode_description));
 
@@ -63,15 +64,27 @@ public class ThemeFragment extends Fragment {
         binding.optionFirst.radioButton.setChecked(index == 0);
         binding.optionSecond.radioButton.setChecked(index == 1);
         binding.optionThird.radioButton.setChecked(index == 2);
+
+        int mode;
+        if (index == 0) {
+            mode = AppCompatDelegate.MODE_NIGHT_NO;
+        } else if (index == 1) {
+            mode = AppCompatDelegate.MODE_NIGHT_YES;
+        } else {
+            mode = AppCompatDelegate.MODE_NIGHT_FOLLOW_SYSTEM;
+        }
+        AppCompatDelegate.setDefaultNightMode(mode);
     }
 
     public void saveSelection() {
         String[] values = getResources().getStringArray(R.array.preference_theme_values);
-        String value = values[0];
+        String value;
         if (binding.optionFirst.radioButton.isChecked()) {
             value = values[1];
         } else if (binding.optionSecond.radioButton.isChecked()) {
             value = values[2];
+        } else {
+            value = values[0];
         }
         viewModel.setTheme(value);
     }
