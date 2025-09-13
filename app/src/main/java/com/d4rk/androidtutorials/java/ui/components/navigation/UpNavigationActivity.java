74713c37b2bef3335b55b467cdac08a8a9eaf43a
Mihai-Cristian Condrea <d4rk7355608@gmail.com>
2025-09-14 00:24:52 +0300

    fix up navigation to respect back stack

--- app/src/main/java/com/d4rk/androidtutorials/java/ui/components/navigation/UpNavigationActivity.java
+++ app/src/main/java/com/d4rk/androidtutorials/java/ui/components/navigation/UpNavigationActivity.java
@@ -1,11 +1,9 @@
 package com.d4rk.androidtutorials.java.ui.components.navigation;
 
-import androidx.core.app.NavUtils;
-
 public abstract class UpNavigationActivity extends BaseActivity {
     @Override
     public boolean onSupportNavigateUp() {
-        NavUtils.navigateUpFromSameTask(this);
+        getOnBackPressedDispatcher().onBackPressed();
         return true;
     }
 }
