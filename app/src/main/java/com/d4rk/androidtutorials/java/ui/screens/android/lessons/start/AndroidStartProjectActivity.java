1fc90f364d687e835a30e5ab0681345c09deb007
D4rK7355608 <d4rk7355608@gmail.com>
2025-10-09 02:27:36 +0300

    Refactor: Remove MaterialToolbar from AndroidStartProjectActivity
    This commit removes the `MaterialToolbar` and its associated logic from `AndroidStartProjectActivity`. The share functionality and toolbar setup have been deleted from the activity's `onCreate` method.
    
    The layout file `activity_android_start_project.xml` has been updated to remove the `MaterialToolbar` view and adjust the `FastScrollScrollView` constraints to fill the parent layout. Additionally, a new `<include>` for a `lesson_section_header` has been added.

--- app/src/main/java/com/d4rk/androidtutorials/java/ui/screens/android/lessons/start/AndroidStartProjectActivity.java
+++ app/src/main/java/com/d4rk/androidtutorials/java/ui/screens/android/lessons/start/AndroidStartProjectActivity.java
@@ -1,10 +1,8 @@
 package com.d4rk.androidtutorials.java.ui.screens.android.lessons.start;
 
-import android.content.Intent;
 import android.os.Bundle;
 import android.text.method.LinkMovementMethod;
 
-import com.d4rk.androidtutorials.java.BuildConfig;
 import com.d4rk.androidtutorials.java.R;
 import com.d4rk.androidtutorials.java.ads.AdUtils;
 import com.d4rk.androidtutorials.java.databinding.ActivityAndroidStartProjectBinding;
@@ -22,20 +20,6 @@ public class AndroidStartProjectActivity extends UpNavigationActivity {
         ActivityAndroidStartProjectBinding binding = ActivityAndroidStartProjectBinding.inflate(getLayoutInflater());
         setContentView(binding.getRoot());
         EdgeToEdgeHelper.applyEdgeToEdge(getWindow(), binding.getRoot());
-        setSupportActionBar(binding.topAppBar);
-        binding.topAppBar.setNavigationOnClickListener(v -> getOnBackPressedDispatcher().onBackPressed());
-        binding.topAppBar.setOnMenuItemClickListener(item -> {
-            if (item.getItemId() == R.id.action_share) {
-                Intent sharingIntent = new Intent(Intent.ACTION_SEND);
-                sharingIntent.setType("text/plain");
-                String shareLink = "https://play.google.com/store/apps/details?id=" + BuildConfig.APPLICATION_ID;
-                sharingIntent.putExtra(Intent.EXTRA_TEXT, shareLink);
-                sharingIntent.putExtra(Intent.EXTRA_SUBJECT, getString(R.string.share_subject));
-                startActivity(Intent.createChooser(sharingIntent, getString(R.string.share_using)));
-                return true;
-            }
-            return false;
-        });
         AdUtils.loadBanner(binding.adViewBottom);
         AdUtils.loadBanner(binding.adView);
         new FastScrollerBuilder(binding.scrollView).useMd2Style().build();
