645ea14025521217c5fde70bfdf52b4c11025095
Mihai-Cristian Condrea <d4rk7355608@gmail.com>
2025-10-08 14:04:51 +0300

    Refactor spinner lesson to use shared scroll scaffold

--- app/src/main/java/com/d4rk/androidtutorials/java/ui/screens/android/lessons/views/spinner/SpinnerActivity.java
+++ app/src/main/java/com/d4rk/androidtutorials/java/ui/screens/android/lessons/views/spinner/SpinnerActivity.java
@@ -14,6 +14,7 @@ import androidx.core.view.WindowCompat;
 import com.d4rk.androidtutorials.java.R;
 import com.d4rk.androidtutorials.java.ads.AdUtils;
 import com.d4rk.androidtutorials.java.databinding.ActivitySpinnerBinding;
+import com.d4rk.androidtutorials.java.databinding.ActivitySpinnerContentBinding;
 import com.d4rk.androidtutorials.java.ui.components.navigation.UpNavigationActivity;
 import com.d4rk.androidtutorials.java.utils.EdgeToEdgeHelper;
 
@@ -22,6 +23,7 @@ import me.zhanghai.android.fastscroll.FastScrollerBuilder;
 public class SpinnerActivity extends UpNavigationActivity {
     private final Handler handler = new Handler(Looper.getMainLooper());
     private ActivitySpinnerBinding binding;
+    private ActivitySpinnerContentBinding contentBinding;
 
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
@@ -30,12 +32,19 @@ public class SpinnerActivity extends UpNavigationActivity {
         binding = ActivitySpinnerBinding.inflate(getLayoutInflater());
         setContentView(binding.getRoot());
         EdgeToEdgeHelper.applyEdgeToEdge(getWindow(), binding.getRoot());
-        binding.descriptionSection.descriptionHeader.getRoot().setText(R.string.description);
-        binding.layoutPreviewHeader.getRoot().setText(R.string.layout_preview);
-        binding.descriptionSection.descriptionText.setText(R.string.summary_spinner);
-        AdUtils.loadBanner(binding.descriptionSection.adView);
 
-        new FastScrollerBuilder(binding.scrollView).useMd2Style().build();
+        contentBinding = ActivitySpinnerContentBinding.inflate(
+                getLayoutInflater(),
+                binding.lessonScaffold.lessonContentContainer,
+                true
+        );
+
+        contentBinding.descriptionSection.descriptionHeader.getRoot().setText(R.string.description);
+        contentBinding.layoutPreviewHeader.getRoot().setText(R.string.layout_preview);
+        contentBinding.descriptionSection.descriptionText.setText(R.string.summary_spinner);
+        AdUtils.loadBanner(contentBinding.descriptionSection.adView);
+
+        new FastScrollerBuilder(binding.lessonScaffold.lessonScrollView).useMd2Style().build();
 
         ArrayAdapter<CharSequence> adapter = ArrayAdapter.createFromResource(
                 this,
@@ -43,29 +52,33 @@ public class SpinnerActivity extends UpNavigationActivity {
                 android.R.layout.simple_spinner_item
         );
         adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
-        binding.spinnerOptions.setAdapter(adapter);
+        contentBinding.spinnerOptions.setAdapter(adapter);
         if (adapter.getCount() > 0) {
             CharSequence firstItem = adapter.getItem(0);
             if (firstItem != null) {
-                binding.textSelectedItem.setText(getString(R.string.spinner_selected_format, firstItem));
+                contentBinding.textSelectedItem.setText(
+                        getString(R.string.spinner_selected_format, firstItem)
+                );
             }
         }
-        binding.spinnerOptions.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
+        contentBinding.spinnerOptions.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
             @Override
             public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
                 CharSequence item = (CharSequence) parent.getItemAtPosition(position);
-                binding.textSelectedItem.setText(getString(R.string.spinner_selected_format, item));
+                contentBinding.textSelectedItem.setText(
+                        getString(R.string.spinner_selected_format, item)
+                );
             }
 
             @Override
             public void onNothingSelected(AdapterView<?> parent) {
-                binding.textSelectedItem.setText(R.string.spinner_selected_none);
+                contentBinding.textSelectedItem.setText(R.string.spinner_selected_none);
             }
         });
 
-        binding.floatingButtonShowSyntax.setOnClickListener(v ->
+        binding.lessonScaffold.lessonShowCodeFab.setOnClickListener(v ->
                 startActivity(new Intent(this, SpinnerCodeActivity.class)));
-        handler.postDelayed(() -> binding.floatingButtonShowSyntax.shrink(), 5000);
+        handler.postDelayed(() -> binding.lessonScaffold.lessonShowCodeFab.shrink(), 5000);
     }
 
     @Override
